2df1bdb710308cb4302002ecf485eaed1ae2ac19
manuelOttlik <manuel.ottlik@web.de>
2018-05-14 21:34:17 +0200

    added config.json for fields

--- index.php
+++ index.php
@@ -5,12 +5,7 @@
 <img src='img/background.jpg' />
 <?php
 error_reporting(0);
-$feldarray = array(
-	"1" => array("news1schlag", "news1spruch", "news1text1", "news1text2"),
-	"2" => array("news2schlag", "news2text1"),
-	"3" => array("news3schlag", "news3text1", "news4schlag", "news4text1"),
-	"4" => array("preis",  "preisinfo1a", "preisinfo1b", "preisinfo2a", "Preisinfo2b", "preisinfo3a", "preisinfo3b", "preisinfo4a", "preisinfo4b", "preisinfo5a", "preisinfo5b")
-);
+$feldarray = json_encode(file_get_contents('config.json'), true);
 
 foreach($feldarray as $gruppe => $felder) {
 	if($_GET['gruppe'] == $gruppe) {
